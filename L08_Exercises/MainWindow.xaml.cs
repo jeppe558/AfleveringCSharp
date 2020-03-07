b8008ffef2aa27ca353a91c28db34ccd36881047
jeppe558 <jeppe558@gmail.com>
2020-03-07 09:28:35 +0100

    Aflevering Jeppe Flarup

---
+++ L08_Exercises/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using L08_Exercises.model;
+using Microsoft.Win32;
+
+namespace L08_Exercises
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<User> _users = new ObservableCollection<User>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                Filter = "csv files (*.csv)|*.csv",
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _users = User.ReadCsvFile(openFileDialog.FileName);
+                UserListBox.ItemsSource = _users;
+                StatusBarItemLastTimeLoaded.Content = "Last time loaded: " + DateTime.Now.ToShortTimeString();
+                StatusBarItemItemsCount.Content = "Number of items in listbox: " + _users.Count;
+            }
+        }
+
+        private void UserSelected(object sender, SelectionChangedEventArgs e)
+        {
+            var i = ((ListBox) sender).SelectedIndex;
+            DataContext = _users[i];
+            
+        }
+
+        private void TextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (UserListBox.SelectedItem == null)
+            {
+                return;
+            }
+            var o = ((TextBox) sender);
+            var user = _users[UserListBox.SelectedIndex];
+            var oName = o.Name.Replace("TextBox", "");
+            try
+            {
+                switch (oName)
+                {
+                    case "Id":
+                        user.Id = o.Text;
+                        break;
+                    case "Name":
+                        user.Name = o.Text;
+                        break;
+                    case "Age":
+                        user.Age = int.Parse(o.Text);
+                        break;
+                    case "Score":
+                        user.Score = int.Parse(o.Text);
+                        break;
+                }
+
+                UserListBox.Items.Refresh();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+
+    }
+    
+}
---
+++ L08_Exercises/model/User.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+
+namespace L08_Exercises.model
+{
+    public class User
+    {
+        public User(string data)
+        {
+            var I = data.Split(';');
+            Id = I[0];
+            Name = I[1];
+            Age = int.Parse(I[2]);
+            Score = int.Parse(I[3]);
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Score { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"ID: {Id}, {Name}");
+        }
+
+        public static ObservableCollection<User> ReadCsvFile(string filename)
+        {
+            var list = new ObservableCollection<User>();
+            using (var file = new StreamReader(filename))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    var u = new User(line);
+                    list.Add(u);
+                }
+            }
+            return list;
+        }
+
+
+    }
+}
